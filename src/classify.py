bacf5af3ea21e5b20216a39ee608f164a47ca189
Connor Boyle <connor.boyle@sagebase.org>
2021-04-19 21:22:12 -0700

    Load Data

--- src/classify.py
+++ src/classify.py
@@ -1,9 +1,34 @@
 import argparse
+import numpy as np
+import csv
+from sklearn.dummy import DummyClassifier
 
 
-def classify(records, model=None):
-    for record in records:
-        print(record.strip('\n'))
+def load_test_data(test_file):
+    # FIXME: This is kind of janky; ideally we could use a CONLL parser
+    tweets = []
+    tweet_ids = []
+    tweet = []
+    for line in test_file:
+        if line.strip() and line.split()[0] == 'meta' and not tweet:
+            tweet_ids.append(line.strip().split('\t')[1])
+        elif line.strip():
+            tweet.append(tuple(line.strip().split('\t')))
+        elif tweet:
+            tweets.append(tuple(tweet))
+            tweet = []
+    if tweet:
+        tweets.append(tuple(tweet))
+    return np.array(tweet_ids), np.array(tweets)
+
+
+def output_predictions(X_ids, z, output_file):
+    output_csv = csv.writer(output_file, quoting=csv.QUOTE_MINIMAL)
+    output_csv.writerow(['Uid', 'Sentiment'])
+    for i in range(len(X_ids)):
+        X_id = X_ids[i]
+        guess = z[i]
+        output_csv.writerow([X_id, guess])
 
 
 if __name__ == '__main__':
@@ -11,10 +36,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='classify records in a test set')
     parser.add_argument('--model-file', type=argparse.FileType('rb'),
                         help='path to saved model to use in classification')
-    parser.add_argument('--test-data', type=argparse.FileType('r'),
-                        help='path to testing data file')
+    parser.add_argument('--test-file', type=argparse.FileType('r'),
+                        help='path to unlabelled testing data file')
     parser.add_argument('--output-file', type=argparse.FileType('w'),
                         help='path to output file')
-
     args = parser.parse_args()
-    classify(args.test_data)
+
+    # Load test data
+    X_ids, X = load_test_data(args.test_file)
+
+    # TODO: load real model from file
+    classifier = DummyClassifier(strategy='constant', constant='positive')
+    classifier.fit([''], ['positive'])
+    z = classifier.predict(X)
+
+    output_predictions(X_ids, z, args.output_file)
