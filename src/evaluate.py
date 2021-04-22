c21b269a0f18285eb9f997b6f6bd250e6fbf443d
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2021-04-22 02:46:38 -0400

    use sklearn f1 function which catches div by 0 errors

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,12 +1,11 @@
 # python3 calculate_f1.py gold_file model_output > results.txt
 
 import sys
-
-def calc_f1(precision, recall):
-    return(100*2*precision*recall/(precision+recall))
+from sklearn.metrics import f1_score, precision_recall_fscore_support
 
 gold_file_path = sys.argv[1]
 model_output_path = sys.argv[2]
+
 
 
 confusion = {}
@@ -24,6 +23,7 @@ for line in gold_lines:
         id,sentiment = line.strip().split(',')
         gold_results[id] = sentiment
 
+model_results = {}
 model_output = open(model_output_path, "r")
 model_lines = model_output.readlines()
 for line in model_lines:
@@ -31,39 +31,24 @@ for line in model_lines:
         continue
     else:
         id,sentiment = line.strip().split(',')
+        model_results[id] = sentiment
         correct_sentiment = gold_results[id]
         confusion[correct_sentiment][sentiment] += 1
 
-pos_count = sum(confusion['positive'].values())
-neu_count = sum(confusion['neutral'].values())
-neg_count = sum(confusion['negative'].values())
-total_count = pos_count + neu_count + neg_count
+gold_predictions = [i[1] for i in sorted(gold_results.items())]
+model_predictions = [i[1] for i in sorted(model_results.items())]
 
-pos_predicted = confusion['positive']['positive'] + confusion['neutral']['positive'] + confusion['negative']['positive']
-neu_predicted = confusion['positive']['neutral'] + confusion['neutral']['neutral'] + confusion['negative']['neutral']
-neg_predicted = confusion['positive']['negative'] + confusion['neutral']['negative'] + confusion['negative']['negative']
+f1_weighted = f1_score(gold_predictions, model_predictions, average='weighted')
+precision, recall, f1, _ = precision_recall_fscore_support(gold_predictions,model_predictions,labels=['positive', 'neutral', 'negative'])
+pos_precision, neu_precision, neg_precision = precision
+pos_recall, neu_recall, neg_recall = recall
+pos_f1, neu_f1, neg_f1 = f1
 
-pos_recall = confusion['positive']['positive']/pos_count
-neu_recall = confusion['neutral']['neutral']/neu_count
-neg_recall = confusion['negative']['negative']/neg_count
-
-pos_precision = confusion['positive']['positive']/pos_predicted
-neu_precision = confusion['neutral']['neutral']/neu_predicted
-neg_precision = confusion['negative']['negative']/neg_predicted
-
-f1_pos = calc_f1(pos_precision, pos_recall)
-f1_neu = calc_f1(neu_precision, neu_recall)
-f1_neg = calc_f1(neg_precision, neg_recall)
-
-f1_pos_weighted = f1_pos * pos_count / total_count
-f1_neu_weighted = f1_neu * neu_count / total_count
-f1_neg_weighted = f1_neg * neg_count / total_count
-f1_weighted = f1_pos_weighted + f1_neu_weighted + f1_neg_weighted
 
 print(f"Weighted F1 score: {f1_weighted}")
-print(f"Positive: Precision={pos_precision} Recall={pos_recall} F1={f1_pos}")
-print(f"Neutral: Precision={neu_precision} Recall={neu_recall} F1={f1_neu}")
-print(f"Negative: Precision={neg_precision} Recall={neg_recall} F1={f1_neg}")
+print(f"Positive: Precision={pos_precision} Recall={pos_recall} F1={pos_f1}")
+print(f"Neutral: Precision={neu_precision} Recall={neu_recall} F1={neu_f1}")
+print(f"Negative: Precision={neg_precision} Recall={neg_recall} F1={neg_f1}")
 print()
 
 
