ff7a676eeeca92a83630872a278907ec66b1b020
Connor Boyle <connor.bo@gmail.com>
2021-04-24 13:34:30 -0700

    Get Trainer working okay-ish

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import SGDClassifier
 
 
 '''
-python train.py --train-file data/Semeval_2020_task9_data/Spanglish/Spanglish_train.conll --model-file model.joblib
+python src/train.py --train-file data/Semeval_2020_task9_data/Spanglish/Spanglish_train.conll --model-file model.joblib
 '''
 
 
@@ -41,11 +41,31 @@ if __name__ == '__main__':
     parser.add_argument('--train-file',
                         type=argparse.FileType('r', encoding='latin-1'),
                         help='path to unlabelled testing data file')
+    parser.add_argument('--dev-file',
+                        type=argparse.FileType('r', encoding='latin-1'),
+                        help='path to unlabelled testing data file')
     args = parser.parse_args()
 
-    tweet_ids, tweets, tweet_sentiments = load_train_data(args.train_file)
+    # Get training tweets from file
+    train_ids, train_tweets, train_sentiments = \
+        load_train_data(args.train_file)
 
+    # Transform tweets into sparse vectors
     vectorizer = CountVectorizer(
-        ngram_range=(1, 3)
+        ngram_range=(1, 3),
+        tokenizer=lambda sequence: [token for token, language in sequence] + ([''] * 3) + [language for token, language in sequence],
+        preprocessor=lambda token: token
     )
+    train_vectors = vectorizer.fit_transform(train_tweets)
+
+    # Train model
     model = SGDClassifier()
+    model.fit(train_vectors, train_sentiments)
+
+    # Save model to joblib
+    joblib.dump((vectorizer, model), args.model_file)
+
+    # Run model on test data
+    # dev_ids, dev_tweets, dev_sentiments = load_train_data(args.dev_file)
+    # dev_vectors = vectorizer.transform(dev_tweets)
+    # dev_predictions = model.predict(dev_vectors)
