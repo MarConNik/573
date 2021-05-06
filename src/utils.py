da1458046b2488911417d073cf7d9df894807cde
Connor Boyle <connor.bo@gmail.com>
2021-05-05 19:53:03 -0700

    Create get_dataloaders() method
    Pretty straightforwardly followed along with the code in the tutorial: http://mccormickml.com/2019/07/22/BERT-fine-tuning/

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+from torch.utils.data import TensorDataset, random_split, DataLoader, RandomSampler, SequentialSampler
+
+DEFAULT_TRAIN_SHARE = 0.90
 
 
 class Preprocessor:
@@ -41,3 +44,27 @@ def load_train_data(training_file):
     if tweet:
         tweets.append(tuple(tweet))
     return np.array(tweet_ids), np.array(tweets), np.array(sentiments)
+
+
+def get_dataloaders(input_ids, attention_masks, sentiment_labels, batch_size: int, train_share=DEFAULT_TRAIN_SHARE):
+    # Split dataset into training and validation subsets
+    dataset = TensorDataset(input_ids, attention_masks, sentiment_labels)
+    training_size = int(train_share * len(dataset))
+    validation_size = len(dataset) - training_size
+    training_dataset, validation_dataset = random_split(dataset, [training_size, validation_size])
+
+    # Create data loaders; training data is loaded in random order
+    # NOTE: "validation" should be a subset of what was given in the "training" file; it has nothing to do with the
+    # contents of the "dev" file, which is used for intermediate model evaluation
+    training_dataloader = DataLoader(
+        training_dataset,
+        sampler=RandomSampler(training_dataset),
+        batch_size=batch_size
+    )
+    validation_dataloader = DataLoader(
+        validation_dataset,
+        sampler=SequentialSampler(validation_dataset),
+        batch_size=batch_size
+    )
+
+    return training_dataloader, validation_dataloader
