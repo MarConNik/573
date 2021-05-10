cec537e1ee5581388c66c0b5d577cede78987c56
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2021-05-10 01:38:59 -0400

    adds in path to output model

--- D3.cmd
+++ D3.cmd
@@ -4,5 +4,5 @@ notification = complete
 transfer_executable = false
 request_memory = 2*1024
 request_GPUs = 1
-arguments = "outputs/baseline/MODEL_DIR_NAME_HERE data/Semeval_2020_task9_data/Spanglish/Spanglish_dev.conll outputs/D3/model_predictions.txt data/Semeval_2020_task9_data/Spanglish/test/Spanglish_dev_gold_labels.conll results/D3_scores.OUT"
+arguments = "outputs/D3/Spanglish-ModelV3.2/ data/Semeval_2020_task9_data/Spanglish/Spanglish_dev.conll outputs/D3/model_predictions.txt data/Semeval_2020_task9_data/Spanglish/test/Spanglish_dev_gold_labels.conll results/D3_scores.OUT"
 queue
