5d8b903e1c5a1ab64da6741d1a0a0d9a09324bcf
Martin Horst <martin.horst@gettyimages.com>
2021-05-09 21:57:48 -0700

    changed condor file and run.sh file for BERT model

--- condor.cmd
+++ condor.cmd
@@ -3,5 +3,6 @@ getenv     = true
 notification = complete
 transfer_executable = false
 request_memory = 2*1024
-arguments = "data/Semeval_2020_task9_data/Spanglish/Spanglish_train.conll model.joblib data/Semeval_2020_task9_data/Spanglish/Spanglish_dev.conll data/Semeval_2020_task9_data/Spanglish/test/Spanglish_dev_gold_labels.conll result.txt evaluation.txt"
+request_GPUs = 1
+arguments = "outputs/baseline/MODEL_NAME_HERE data/Semeval_2020_task9_data/Spanglish/Spanglish_dev.conll outputs/D3/model_predictions.txt data/Semeval_2020_task9_data/Spanglish/test/Spanglish_dev_gold_labels.conll results/D3_scores.OUT"
 queue
