99906f0297c377342a61916d4285804ebb1ceb0a
Connor Boyle <connor.bo@gmail.com>
2021-05-30 18:38:30 -0700

    Add Separate Run for Hinglish Dev & Eval

--- src/run_d4.sh
+++ src/run_d4.sh
@@ -19,7 +19,12 @@ HINGLISH_MODEL_DIR="$OUTPUTS_DIR/hinglish-model"
 HINGLISH_OUTPUT_FILE="$OUTPUTS_DIR/hinglish-test.results"
 HINGLISH_EVAL_FILE="$RESULTS_DIR/hinglish-eval.txt"
 
-# Train, run, and evaluate Hinglish model
+# Train, run, and evaulate Hinglish model (dev)
+python src/train.py --train-file "$HINGLISH_TRAIN_FILE" --model-directory "$HINGLISH_MODEL_DIR"
+python src/classify.py --model-directory "$HINGLISH_MODEL_DIR" --test-file "$HINGLISH_DEV_FILE" --output-file $HINGLISH_OUTPUT_FILE
+python src/evaluate.py --train-file "$HINGLISH_GOLD_STANDARD" "$HINGLISH_OUTPUT_FILE" > "$HINGLISH_EVAL_FILE"
+
+# Train, run, and evaluate Hinglish model (eval)
 python src/train.py --train-file "$HINGLISH_TRAIN_FILE" --dev-file $HINGLISH_DEV_FILE --model-directory "$HINGLISH_MODEL_DIR"
 python src/classify.py --model-directory "$HINGLISH_MODEL_DIR" --test-file "$HINGLISH_TEST_FILE" --output-file $HINGLISH_OUTPUT_FILE
 python src/evaluate.py --train-file "$HINGLISH_GOLD_STANDARD" "$HINGLISH_OUTPUT_FILE" > "$HINGLISH_EVAL_FILE"
