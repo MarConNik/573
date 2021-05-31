e4c54f92fd08b6716fa209a1fa05b093db25898c
Connor Boyle <connor.bo@gmail.com>
2021-05-30 20:44:01 -0700

    Change Memory Request to 20 GB

--- D4.cmd
+++ D4.cmd
@@ -2,6 +2,6 @@ executable = src/scripts/run_d4.sh
 getenv     = true
 notification = complete
 transfer_executable = false
-request_memory = 2*1024
+request_memory = 20*1024
 request_GPUs = 1
 queue
