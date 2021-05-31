0ab66812ebfe49b784784c7f886319cefe54b11e
Connor Boyle <connor.bo@gmail.com>
2021-05-30 19:18:40 -0700

    Make run_eval.sh not Evaluate for Spanglish

--- src/scripts/run_eval.sh
+++ src/scripts/run_eval.sh
@@ -5,4 +5,7 @@ EVAL_FILE="$RESULTS_DIR/${LANG_PAIR}-eval.txt"
 # Train, classify, and evaluate on Eval set
 python src/train.py --train-file "$TRAIN_FILE" --dev-file $DEV_FILE --model-directory "$MODEL_DIR"
 python src/classify.py --model-directory "$MODEL_DIR" --test-file "$TEST_FILE" --output-file $OUTPUT_FILE
-python src/evaluate.py --train-file "$GOLD_STANDARD" "$OUTPUT_FILE" > "$EVAL_FILE"
+if [[ $LANG_PAIR = 'hinglish']]
+then
+  python src/evaluate.py --train-file "$GOLD_STANDARD" "$OUTPUT_FILE" > "$EVAL_FILE"
+fi
