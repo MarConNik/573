a62fe2ddabf323d2f2f3ddee810bd29bedbd8aec
Connor Boyle <connor.bo@gmail.com>
2021-05-30 19:24:12 -0700

    Add Status Echos for Spanglish/Hinglish Train Scripts

--- src/scripts/run_dev.sh
+++ src/scripts/run_dev.sh
@@ -2,7 +2,13 @@ MODEL_DIR="$OUTPUTS_DIR/${LANG_PAIR}-dev-model"
 OUTPUT_FILE="$OUTPUTS_DIR/${LANG_PAIR}-dev.results"
 EVAL_FILE="$RESULTS_DIR/${LANG_PAIR}-dev.txt"
 
+echo "Running dev for ${LANG_PAIR}"
+
 # Train, classify, and evaluate on Dev dataset
+echo "training..."
 python src/train.py --train-file "$TRAIN_FILE" --model-directory "$MODEL_DIR"
+echo "classifying..."
 python src/classify.py --model-directory "$MODEL_DIR" --test-file "$TEST_FILE" --output-file $OUTPUT_FILE
+echo "evaluating..."
 python src/evaluate.py --train-file "$GOLD_STANDARD" "$OUTPUT_FILE" > "$EVAL_FILE"
+echo "complete!"
--- src/scripts/run_eval.sh
+++ src/scripts/run_eval.sh
@@ -2,10 +2,16 @@ MODEL_DIR="$OUTPUTS_DIR/${LANG_PAIR}-eval-model"
 OUTPUT_FILE="$OUTPUTS_DIR/${LANG_PAIR}-eval.results"
 EVAL_FILE="$RESULTS_DIR/${LANG_PAIR}-eval.txt"
 
+echo "Running eval for ${LANG_PAIR}"
+
 # Train, classify, and evaluate on Eval set
+echo "training..."
 python src/train.py --train-file "$TRAIN_FILE" --dev-file $DEV_FILE --model-directory "$MODEL_DIR"
+echo "classifying..."
 python src/classify.py --model-directory "$MODEL_DIR" --test-file "$TEST_FILE" --output-file $OUTPUT_FILE
 if [[ $LANG_PAIR = 'hinglish']]
 then
+  echo "evaluating..."
   python src/evaluate.py --train-file "$GOLD_STANDARD" "$OUTPUT_FILE" > "$EVAL_FILE"
 fi
+echo "complete!"
