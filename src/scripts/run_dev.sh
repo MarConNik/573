33e004bd3445f5b085a5bb4deafb0a776c5bb4cf
Connor Boyle <connor.bo@gmail.com>
2021-05-30 19:31:54 -0700

    Add FINAL Model Dir, Fix if Statement

--- src/scripts/run_dev.sh
+++ src/scripts/run_dev.sh
@@ -8,7 +8,7 @@ echo "Running dev for ${LANG_PAIR}"
 echo "training..."
 python src/train.py --train-file "$TRAIN_FILE" --model-directory "$MODEL_DIR"
 echo "classifying..."
-python src/classify.py --model-directory "$MODEL_DIR" --test-file "$TEST_FILE" --output-file $OUTPUT_FILE
+python src/classify.py --model-directory "${MODEL_DIR}/FINAL" --test-file "$TEST_FILE" --output-file $OUTPUT_FILE
 echo "evaluating..."
 python src/evaluate.py --train-file "$GOLD_STANDARD" "$OUTPUT_FILE" > "$EVAL_FILE"
 echo "complete!"
--- src/scripts/run_eval.sh
+++ src/scripts/run_eval.sh
@@ -8,8 +8,8 @@ echo "Running eval for ${LANG_PAIR}"
 echo "training..."
 python src/train.py --train-file "$TRAIN_FILE" --dev-file $DEV_FILE --model-directory "$MODEL_DIR"
 echo "classifying..."
-python src/classify.py --model-directory "$MODEL_DIR" --test-file "$TEST_FILE" --output-file $OUTPUT_FILE
-if [[ $LANG_PAIR = 'hinglish']]
+python src/classify.py --model-directory "${MODEL_DIR}/FINAL" --test-file "$TEST_FILE" --output-file $OUTPUT_FILE
+if [[ "$LANG_PAIR" == "hinglish" ]]
 then
   echo "evaluating..."
   python src/evaluate.py --train-file "$GOLD_STANDARD" "$OUTPUT_FILE" > "$EVAL_FILE"
